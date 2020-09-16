fdc1ed4720498372e1d2891f54314f25ad603cd3
sreejithkrajan <71411170+sreejithkrajan@users.noreply.github.com>
2020-09-16 18:54:52 -0400

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -1,5 +1,5 @@
   // **** Your JavaScript code goes here ****
-  d3.csv('/assets/data/experience.csv').then(function(dataset) {	 
+  d3.csv('./assets/data/experience.csv').then(function(dataset) {	 
 	  console.log('newfile');
 	  console.log(dataset);
 	});
