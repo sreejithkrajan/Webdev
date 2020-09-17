182526a2022efa3378484e5963a8b114c6639e40
sreejithkrajan <71411170+sreejithkrajan@users.noreply.github.com>
2020-09-17 14:15:20 -0400

    edit2

--- assets/js/pageload/index.js
+++ assets/js/pageload/index.js
@@ -3,6 +3,11 @@
 	 educationUpdate(dataset);
 	});
 	
+	
+	d3.csv('./assets/data/experience.csv').then(function(dataset) { 		
+		experienceUpdate(dataset);		
+	});
+	
 function educationUpdate(dataset){
 	const eduList =d3.select("#education-div").selectAll("div")
 		.data(dataset)
@@ -34,3 +39,32 @@ function educationUpdate(dataset){
 }
 
 
+function experienceUpdate(dataset){
+	const eduList =d3.select("#experience-div").selectAll("div")
+		.data(dataset)
+		.enter()
+		.append("div")
+		.attr("class","tl-item pub-item with-icon");		
+		
+		
+	eduList.append("span")
+		.attr("class","item-section")
+		.text(function(d, i) { return d.Year});
+		
+	eduList.append("div")
+		.attr("class","elem-wrapper")
+		.append("i")
+		.attr("class",function(d, i) { return d.icon});
+		
+	const edItem=eduList.append("div")
+						.attr("class","content-wrapper");
+	
+	edItem.append("h3")
+		.attr("class","title")
+		.text(function(d, i) { return d.Position});
+		
+	edItem.append("div")
+		.attr("class","description")
+		.append("p")
+		.text(function(d, i) { return d.Company});
+}
