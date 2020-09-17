04e12317b50f01f582b9a4fda555da197227239f
sreejithkrajan <71411170+sreejithkrajan@users.noreply.github.com>
2020-09-16 20:14:13 -0400

    education

--- assets/js/pageload/index.js
+++ assets/js/pageload/index.js
@@ -1,7 +1,36 @@
   // **** Your JavaScript code goes here ****
-  d3.csv('./assets/data/experience.csv').then(function(dataset) {	 
-	  console.log('newfile');
-	  console.log(dataset);
+  d3.csv('./assets/data/education.csv').then(function(dataset) {	 
+	 educationUpdate(dataset);
 	});
+	
+function educationUpdate(dataset){
+	const eduList =d3.select("#education-div").selectAll("div")
+		.data(dataset)
+		.enter()
+		.append("div")
+		.attr("class","tl-item pub-item with-icon");		
+		
+		
+	eduList.append("span")
+		.attr("class","item-section")
+		.text(function(d, i) { return d.Year});
+		
+	eduList.append("div")
+		.attr("class","elem-wrapper")
+		.append("i")
+		.attr("class","oli oli-diploma");
+		
+	const edItem=eduList.append("div")
+						.attr("class","content-wrapper");
+	
+	edItem.append("h3")
+		.attr("class","title")
+		.text(function(d, i) { return d.Degree});
+		
+	edItem.append("div")
+		.attr("class","description")
+		.append("p")
+		.text(function(d, i) { return d.University});
+}
 
 
