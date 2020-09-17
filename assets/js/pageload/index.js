e2c529fc0813729125dd971f66e1401069fcde77
sreejithkrajan <71411170+sreejithkrajan@users.noreply.github.com>
2020-09-17 15:51:55 -0400

    publications

--- assets/js/pageload/index.js
+++ assets/js/pageload/index.js
@@ -8,6 +8,12 @@
 		experienceUpdate(dataset);		
 	});
 	
+	
+d3.csv('./assets/data/publications.csv').then(function(dataset) { 		
+		publicationsUpdate(dataset);
+		
+	});
+	
 function educationUpdate(dataset){
 	const eduList =d3.select("#education-div").selectAll("div")
 		.data(dataset)
@@ -67,4 +73,44 @@ function experienceUpdate(dataset){
 		.attr("class","description")
 		.append("p")
 		.text(function(d, i) { return d.Company});
+}
+
+
+function publicationsUpdate(dataset){
+	const pubList =d3.select("#recent-pub-div").selectAll("div")
+		.data(dataset)
+		.enter()
+		.append("div")
+		.attr("class","pub-item with-icon");
+		
+		pubList.append("div")
+		.attr("class","elem-wrapper")
+		.append("i")
+		.attr("class","oli oli-bookmark");
+		
+		const pubItem=pubList.append("div")
+						.attr("class","content-wrapper");
+						
+		pubItem.append("h3")
+		.attr("class","title")
+		.text(function(d, i) { return d.Title});
+		
+		const pubDetails=pubItem.append("div")
+					.attr("class","description");
+		
+		pubDetails.append("div")
+		.attr("class","citation")
+		.text(function(d, i) { return d.Citation});
+		
+		pubDetails.append("div")
+		.attr("class","citation")
+		.text(function(d, i) { return d.Authors});
+		
+		pubDetails.append("a")
+		.attr("class","link-with-icon")
+		.attr("href",function(d, i) { return d.Link})
+		.text("Publisher's website")
+		.append("i")
+		.attr("class","fa fa-external-link");			
+						
 }
